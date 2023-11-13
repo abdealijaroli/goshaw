eedfd931adbec2097182a8dd337c44194e761946
abdealijaroli <abdealiburu03@gmail.com>
2023-11-13 22:17:31 +0530

    file restructure

--- main.go
+++ main.go
@@ -8,14 +8,10 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
+	
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Query().Get("name")
-		if name == "" {
-			name = "aj"
-		}
-				
-		fmt.Fprintf(w, "hello %s", name)
+		fmt.Fprintf(w, "hello %s", r.FormValue("name"))
 	})
 
 	//remove 'localhost' when deploying to prod
